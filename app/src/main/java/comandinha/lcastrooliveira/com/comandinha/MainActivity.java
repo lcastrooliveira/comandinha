8637c1facfee627806b6b23221edeee2fd04ca7d
Lucas Oliveira <lcastro.oliveira@gmail.com>
2018-11-18 13:04:22 -0200

    termina o design da tela inicial~

--- app/src/main/java/comandinha/lcastrooliveira/com/comandinha/MainActivity.java
+++ app/src/main/java/comandinha/lcastrooliveira/com/comandinha/MainActivity.java
@@ -1,13 +1,43 @@
 package comandinha.lcastrooliveira.com.comandinha;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import comandinha.lcastrooliveira.com.comandinha.adapters.ItemComandinhaAdapter;
+import comandinha.lcastrooliveira.com.comandinha.model.ItemComandinha;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewComandinha;
+    private ItemComandinhaAdapter adapter;
+    private FloatingActionButton pedirContaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listViewComandinha = findViewById(R.id.listViewComanda);
+        pedirContaButton = findViewById(R.id.pedirContaButton);
+
+        List<ItemComandinha> itemsComanda = new ArrayList<>();
+        itemsComanda.add(new ItemComandinha("Chopp", 2));
+        itemsComanda.add(new ItemComandinha("Porção", 1));
+
+        adapter = new ItemComandinhaAdapter(itemsComanda, getApplicationContext());
+        listViewComandinha.setAdapter(adapter);
+        pedirContaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "A conta por favor!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/comandinha/lcastrooliveira/com/comandinha/adapters/ItemComandinhaAdapter.java
+++ app/src/main/java/comandinha/lcastrooliveira/com/comandinha/adapters/ItemComandinhaAdapter.java
@@ -1,21 +1,35 @@
 package comandinha.lcastrooliveira.com.comandinha.adapters;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 
-import java.util.ArrayList;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.util.List;
 
 import comandinha.lcastrooliveira.com.comandinha.R;
 import comandinha.lcastrooliveira.com.comandinha.model.ItemComandinha;
 
 public class ItemComandinhaAdapter extends ArrayAdapter<ItemComandinha> implements View.OnClickListener{
 
-    private ArrayList<ItemComandinha> dataSet;
+    private List<ItemComandinha> dataSet;
     Context mContext;
 
+    private static class ViewHolder {
+        TextView itemComandaNomeTextView;
+        TextView itemComandaQuantidadeTextView;
+        ImageButton itemComandaMaisUmButton;
+        ImageButton itemComandaTiraUmButton;
+    }
 
-    public ItemComandinhaAdapter(ArrayList<ItemComandinha> data, Context context) {
+    public ItemComandinhaAdapter(List<ItemComandinha> data, Context context) {
         super(context, R.layout.item_comanda, data);
         this.dataSet = data;
         this.mContext=context;
@@ -24,6 +38,35 @@ public class ItemComandinhaAdapter extends ArrayAdapter<ItemComandinha> implemen
 
     @Override
     public void onClick(View v) {
+        Log.i(ItemComandinha.class.getName(), "Clicou aqui");
+        Toast.makeText(this.mContext, "Lucas", Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ItemComandinha itemComandinha = getItem(position);
+
+        ViewHolder viewHolder;
+
+        if(convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_comanda, parent, false);
+            viewHolder.itemComandaNomeTextView = convertView.findViewById(R.id.itemComandaNomeTextView);
+            viewHolder.itemComandaQuantidadeTextView = convertView.findViewById(R.id.itemComandaQuantidadeTextView);
+            viewHolder.itemComandaMaisUmButton = convertView.findViewById(R.id.itemComandaMaisUmButton);
+            viewHolder.itemComandaTiraUmButton = convertView.findViewById(R.id.itemComandaTiraUmButton);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.itemComandaNomeTextView.setText(itemComandinha.getNome());
+        viewHolder.itemComandaQuantidadeTextView.setText(String.valueOf(itemComandinha.getQuantidade()));
+        viewHolder.itemComandaMaisUmButton.setOnClickListener(this);
+        viewHolder.itemComandaTiraUmButton.setOnClickListener(this);
+
+        return convertView;
     }
 }
